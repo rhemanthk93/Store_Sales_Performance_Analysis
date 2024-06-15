02bdc65c4c672acb4503d3dc0c3ebdd472632d6b
hemanth <rhemanthk93@gmail.com>
2024-06-15 15:31:36 +0800

    resolved cors issue

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from app.config import Config
+from flask_cors import CORS
 
 db = SQLAlchemy()
 
@@ -8,6 +9,7 @@ db = SQLAlchemy()
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
+    CORS(app, resources={r"/*": {"origins": "*"}})
 
     db.init_app(app)
 
